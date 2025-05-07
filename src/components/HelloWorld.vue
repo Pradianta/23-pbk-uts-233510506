8f43970782071ea9ebede0297f19801e606d5c63
Pradianta <shabilun810@gmail.com>
2025-05-07 19:33:57 +0700

    Menambahkan fitur filter kegiatan yang belum selesai dan counter untuk menampilkan jumlah kegiatan yang belum selesai

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const activities = ref([
   { name: 'olahraga', completed: false },
@@ -7,6 +7,16 @@ const activities = ref([
   { name: 'tidur', completed: false }
 ])
 const newActivity = ref('')
+const showUndoneOnly = ref(false)
+
+const filteredActivities = computed(() => {
+  if (showUndoneOnly.value) {
+    return activities.value.filter(activity => !activity.completed)
+  }
+  return activities.value
+})
+
+const undoneCount = computed(() => activities.value.filter(activity => !activity.completed).length)
 
 function addActivity() {
   if (newActivity.value.trim() !== '') {
@@ -18,6 +28,10 @@ function addActivity() {
 function removeActivity(index) {
   activities.value.splice(index, 1)
 }
+
+function toggleShowUndone() {
+  showUndoneOnly.value = !showUndoneOnly.value
+}
 </script>
 
 <template>
@@ -25,9 +39,11 @@ function removeActivity(index) {
     <label for="activityInput">Daftar Kegiatan:</label>
     <input id="activityInput" type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
     <button type="button" @click="addActivity">Submit</button>
+    <button type="button" @click="toggleShowUndone" style="margin-left: 10px;">Undone</button>
+    <div style="margin-top: 10px;">Belum selesai: {{ undoneCount }}</div>
 
     <ul>
-      <li v-for="(activity, index) in activities" :key="index">
+      <li v-for="(activity, index) in filteredActivities" :key="index">
         <input type="checkbox" v-model="activity.completed" />
         <span :style="{ textDecoration: activity.completed ? 'line-through' : 'none' }">{{ activity.name }}</span>
         <button type="button" @click="removeActivity(index)" style="margin-left: 10px;">Hapus</button>
