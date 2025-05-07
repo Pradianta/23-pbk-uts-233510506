2166624e73063e8ae7150fa981be14b100114388
Pradianta <shabilun810@gmail.com>
2025-05-07 19:09:41 +0700

    Menambahkan fitur untuk menampilkan kegiatan yang sudah di tambahkan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { ref } from 'vue'
 
+const activities = ref(['olahraga', 'makan', 'tidur'])
+const newActivity = ref('')
 </script>
 
 <template>
-  
+  <div>
+    <label for="activityInput">Daftar Kegiatan:</label>
+    <input id="activityInput" type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
+    <button type="button">Submit</button>
+
+    <ul>
+      <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
