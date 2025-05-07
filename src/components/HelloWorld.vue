30ca9406f8f46222b2a74ca032d09b1ab1f76bfd
Pradianta <shabilun810@gmail.com>
2025-05-07 19:12:54 +0700

    Menambahkan fitur untuk menambahkan kegiatan ke dalam daftar kegiatan

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,20 @@ import { ref } from 'vue'
 
 const activities = ref(['olahraga', 'makan', 'tidur'])
 const newActivity = ref('')
+
+function addActivity() {
+  if (newActivity.value.trim() !== '') {
+    activities.value.push(newActivity.value.trim())
+    newActivity.value = ''
+  }
+}
 </script>
 
 <template>
   <div>
     <label for="activityInput">Daftar Kegiatan:</label>
     <input id="activityInput" type="text" v-model="newActivity" placeholder="Masukkan kegiatan" />
-    <button type="button">Submit</button>
+    <button type="button" @click="addActivity">Submit</button>
 
     <ul>
       <li v-for="(activity, index) in activities" :key="index">{{ activity }}</li>
